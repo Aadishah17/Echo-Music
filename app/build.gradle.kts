90318be7d72d0551583051bb890ef9f8fb0e3d55
Aditya <adityadav9532@gmail.com>
2026-07-30 07:21:55 +0530

    Version Bump - 5.2.83

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "iad1tya.echo.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 524
-        versionName = "5.2.82"
+        versionCode = 525
+        versionName = "5.2.83"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
