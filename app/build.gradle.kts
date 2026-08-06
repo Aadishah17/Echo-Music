76e429d9a0a45fd5fe9e0244a354377b936393b5
Aditya <adityadav9532@gmail.com>
2026-08-07 03:49:24 +0530

    chore: bump version to 5.2.87

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 527
-        versionName = "5.2.86"
+        versionName = "5.2.87"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
